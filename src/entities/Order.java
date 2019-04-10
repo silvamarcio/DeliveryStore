07eacc4b6de6d54c90615c2fc0bdf7b55f6acdfa
silvamarcio <silva.marcio86@hotmail.com>
2019-04-10 09:40:56 -0300

    read me

---
+++ src/entities/Order.java
@@ -0,0 +1,73 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client;
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+
+	public void addItems(OrderItem item) {
+		items.add(item);
+	}
+	
+	public void removerItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	public double total() {
+		double sum = 0;
+		for(OrderItem or : items) {
+			sum += or.subTotal();
+		}
+		
+		return sum;
+	}
+	
+	
+}
